ad420091216b9f8d191709ac7b8314d66758a2b9
Roman Kruk <wilmlass@gmail.com>
2021-09-03 14:16:15 +0300

    added content buttons

--- src/js/content-grid/content-grid.main.js
+++ src/js/content-grid/content-grid.main.js
@@ -1,21 +1,23 @@
 import ApiService from "../api-service/api-service";
 import contentCardsTmp from "../../templates/content-grid";
 import getGenres from "./get-genres";
-import createTrendsMarkup from "./create-markup";
+import createContentMarkup from "./create-markup";
 import updateGenresInfo from "./update-genres-info";
 import updateYearinfo from "./update-year-info";
 import updateRating from "./update-rating";
 const contentCardsRef = document.querySelector(".content__cards");
+const contentBtnListRef = document.querySelector(".content__btn__list");
 const apiService = new ApiService();
 
 getGenres(apiService);
-onload();
+renderContent(apiService.fetchTrend({}));
+contentBtnListRef.addEventListener("click", onContentBtnClick);
 
-async function onload() {
+async function renderContent(api) {
   try {
-    const collection = await apiService.fetchTrend({});
+    const collection = await api;
 
-    createTrendsMarkup(
+    createContentMarkup(
       contentCardsRef,
       collection,
       contentCardsTmp(collection.results)
@@ -25,5 +27,14 @@ async function onload() {
     updateRating();
   } catch (error) {
     console.log(error);
+  }
+}
+
+async function onContentBtnClick(e) {
+  e.preventDefault();
+  if (e.target.dataset.action === "trend") {
+    renderContent(apiService.fetchTrend({}));
+  } else if (e.target.dataset.action === "popular") {
+    renderContent(apiService.fetchPopular({}));
   }
 }
