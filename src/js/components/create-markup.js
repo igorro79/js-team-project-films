8adee63702ae722fd07f54ace33b378268d1f423
Igor Griniov <griniov.igor@gmail.com>
2021-09-08 02:25:38 +0300

    warning massage added on search

--- src/js/components/create-markup.js
+++ src/js/components/create-markup.js
@@ -1,9 +1,26 @@
+import pageInit from "./page-init";
+import ApiService from "../api-service/api-service";
+import { contentCardsRef } from "../content-grid.main";
+
+let badSearchMsg = document.querySelector(".header__warning");
+let body = document.querySelector("BODY");
+const apiService = new ApiService();
+let loaderTeg = document.getElementById("loader");
+
 export default function createContentMarkup(element, collection, template) {
   if (collection.total_results === 0) {
-    alert("По вашему запросу ничего ненайдено. Введите другой запрос");
+    badSearchMsg.removeAttribute("style", "display: none");
+
+    body.addEventListener("click", removeWarning);
+    pageInit(apiService, contentCardsRef);
     return;
   } else if (collection.total_results < 20) {
-    alert("Это всё, что было найдено по вашему запросу");
+    badSearchMsg.removeAttribute("style", "display: none");
   }
   element.innerHTML = template;
+}
+
+function removeWarning() {
+  badSearchMsg.setAttribute("style", "display: none");
+  body.removeEventListener("click", removeWarning);
 }
