4fe259b481156ccdd842b0092647ede05705f8f0
Roman Kruk <wilmlass@gmail.com>
2021-09-13 00:28:13 +0300

    fix bug in update genres fn

--- src/js/components/update-genres-info.js
+++ src/js/components/update-genres-info.js
@@ -9,10 +9,13 @@ export default function updateGenresInfo() {
     let genreName = '';
     let genreIDArr = genreIdRef.textContent.trim().split(',');
 
-    info.forEach(savedGenre => {
-      genreIDArr.filter(genreId => {
+    genreIDArr.filter(genreId => {
+      info.forEach(savedGenre => {
         if (Number(savedGenre.id) === Number(genreId)) {
           return result.push(savedGenre.name);
+        }
+        if (genreId.trim() === savedGenre.name || genreId.trim() === 'Other') {
+          return result.push(genreId.trim());
         }
       });
     });
